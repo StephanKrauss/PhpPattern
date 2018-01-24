d216249abac40c5e3cefee4b344ebbaf682df1b7
Stephan Krauss <info@stephankrauss.de>
2018-01-24 20:16:41 +0100

    einfaches Beispiel eines Decorator Pattern

---
+++ Src/Structural/Decorator/PrintBook.php
@@ -0,0 +1,43 @@
+<?php
+
+	namespace App\Structural\Decorator;
+
+/**
+ * User: Junade Ali
+ * Date: 26/03/2016
+ * Time: 19:21
+ */
+class PrintBook implements Book
+{
+
+    public $eBook;
+
+    public function __construct(string $title, string $author, string $contents)
+    {
+        $this->eBook = new \App\Structural\Decorator\EBook($title, $author, $contents);
+    }
+
+    public function getTitle(): string
+    {
+        return $this->eBook->getTitle();
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->eBook->getAuthor();
+    }
+
+    public function getContents(): string
+    {
+        return $this->eBook->getContents();
+    }
+
+    public function getText(): string
+    {
+        $contents = $this->eBook->getTitle() . " by " . $this->eBook->getAuthor();
+        $contents .= "\n";
+        $contents .= $this->eBook->getContents();
+
+        return $contents;
+    }
+}
